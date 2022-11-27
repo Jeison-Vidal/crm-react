3426a33a05250a744b84cd106060cbd0886fddf3
Jeison-Vidal <j.vidal93@pascualbravo.edu.co>
2022-11-27 14:35:28 -0500

    Modificando estilos de cliententes

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,94 @@
-import React from 'react'
+import { useLoaderData } from "react-router-dom"
+import Cliente from "../components/Cliente";
+export function loader(){
+
+  const clientes = [
+    {
+        id: 1,
+        nombre: 'Juan',
+        telefono: 102013313,
+        email: "juan@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 2,
+        nombre: 'Karen',
+        telefono: 138198313,
+        email: "karen@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 3,
+        nombre: 'Josue',
+        telefono: 31983913,
+        email: "josue@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 4,
+        nombre: 'Miguel',
+        telefono: 319381983,
+        email: "miguel@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+    {
+        id: 5,
+        nombre: 'Pedro',
+        telefono: 1398198938,
+        email: "pedro@juan.com",
+        empresa: 'Codigo Con Juan'
+    },
+];
+  return clientes
+}
 
 function Index() {
+  const clientes = useLoaderData();
+
   return (
-    <div>Index</div>
+    <>
+    <h1 className='font-black text-4xl text-blue-900' >Clientes</h1>
+    <p className='mt-3' >Administrativo</p>
+
+   
+    {clientes.length ? (
+      <table className="w-full bg-white shadow mt-5 table-auto">
+        <thead className="bg-blue-800 text-white0">
+          <tr>
+              <th className="p-2">Clientes</th>
+              <th className="p-2">Contacto</th>
+              
+              <th className="p-2">Acciones</th>
+          </tr>
+          </thead>
+          <tbody>
+
+            {clientes.map(cliente=>(
+                <tr>
+
+                  <td className="p-6">
+                    <Cliente
+                    cliente={cliente}
+                    key={cliente.id}
+                    
+                    />
+                  </td>
+              
+                </tr>
+                
+                
+
+
+            ))}
+          </tbody>
+
+       
+       
+
+
+      </table>
+    ) : (<p className="text-center mt-10">No hay clientes que mostrar</p>) }
+    </>
   )
 }
 
